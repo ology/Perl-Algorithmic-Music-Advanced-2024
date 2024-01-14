5b9e2064e59af1ba5dedb930cf2c93803d2e1a5c
Gene Boggs <gene@ology.net>
2024-01-14 12:52:08 -0800

    Ex 2

---
+++ code/example-2.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Util Music-Duration-Partition); # my local libraries
+
+use Data::Dumper::Compact qw(ddc);
+use MIDI::Util qw(setup_score);
+use Music::Duration::Partition ();
+use Music::Scales qw(get_scale_MIDI);
+use Music::VoiceGen ();
+
+my $score = setup_score();
+
+my $mdp = Music::Duration::Partition->new(
+    size => 8, # number of quarter-note beats (2 measures)
+    pool => [qw(hn dqn qn en)],
+);
+
+my @motifs = $mdp->motifs(4);
+
+my @pitches = (get_scale_MIDI('C', 4, 'major'),
+               get_scale_MIDI('C', 5, 'major'));
+
+my $voice = Music::VoiceGen->new(
+    pitches   => \@pitches,
+    intervals => [qw(-3 -2 -1 1 2 3)],
+);
+
+for my $motif (@motifs) {
+    my @voices = map { $voice->rand } @$motif;
+
+    $mdp->add_to_score($score, $motif, \@voices);
+}
+
+$score->write_score('audio/example-2.mid');
+
